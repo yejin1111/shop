ba09c3c18c1a3bdbff9f1b5edebb0855d93fbdad
yjjang <22sss123@naver.com>
2024-11-22 16:41:02 +0900

    구글계정수정

--- src/main/java/com/shop/service/EmailService.java
+++ src/main/java/com/shop/service/EmailService.java
@@ -1,31 +1,22 @@
 package com.shop.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-
 @Service
-@RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     public void sendEmail(String to, String subject, String body) {
-        MimeMessage message = mailSender.createMimeMessage();
-
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(body, false); // HTML 형식이라면 true로 변경
-
-            mailSender.send(message);
-        } catch (MessagingException e) {
-            throw new IllegalStateException("이메일 전송 중 오류가 발생했습니다.", e);
-        }
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        javaMailSender.send(message);
+        message.setFrom("jyj5120224@gmail.com");         //본인 gmail계정
     }
 }
